ce07c9e6e63f8a2fc1a66c6be8ff83185d3ef801
Ben Greenberg <bsgreenb@gmail.com>
2022-12-10 13:21:31 -0800

    setup favicon for manifest

--- packages/couches-web/gatsby-config.js
+++ packages/couches-web/gatsby-config.js
@@ -20,15 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `FreeCouches`,
+        short_name: `FreeCouches`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#520075`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#520075`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/couch-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
