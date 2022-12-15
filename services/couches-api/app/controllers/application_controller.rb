a3d0a2b9e30f0aeeab94fc609d8aa65c9d61f4e2
Benjamin Greenberg <bsgreenb@gmail.com>
2022-12-15 15:39:56 -0800

    Sendgrid action mailer demo

--- services/couches-api/app/controllers/application_controller.rb
+++ services/couches-api/app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::API
         render json: {"status": "ok", "message": "You're on the FreeCouches API - running Rails version 7.0.3"}
     end
 
+    # TODO: remove this
+    def test_email
+        UserNotifierMailer.send_signup_email('benjamin@freecouches.org').deliver
+    end
+
 end
