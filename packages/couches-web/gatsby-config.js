20f189d0a5b52c0c68232a411d5d8cde999e4dee
Benjamin Greenberg <bsgreenb@gmail.com>
2023-01-09 16:55:38 -0600

    Remove Gatsby Cruft from starter

--- packages/couches-web/gatsby-config.js
+++ packages/couches-web/gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `FreeCouches`,
+    description: `Connecting travelers with free places to stay`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://freecouches.org/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -28,15 +28,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `freecouches`,
+        short_name: `freecouches`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/couch-square.png`, // This path is relative to the root of the site.
       },
     },
   ],
--- packages/couches-web/gatsby-node.js
+++ packages/couches-web/gatsby-node.js
@@ -3,16 +3,3 @@
  *
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
