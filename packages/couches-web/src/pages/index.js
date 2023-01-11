d9bb1d847611a6ddad99d7b64533c3cea9e0e2bb
Benjamin Greenberg <bsgreenb@gmail.com>
2023-01-11 09:11:51 -0600

    Remove unneeded import

--- packages/couches-web/src/pages/index.js
+++ packages/couches-web/src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
 import { getUser, isLoggedIn } from "../services/auth"
 import Layout from "../components/layout"
